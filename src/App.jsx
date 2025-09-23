d5e3016312b66e6a9a54ed00930f352c276a951d
mfurqaann <furqan1061@gmail.com>
2025-09-23 17:24:44 +0700

    feat(twosum): create logic in the component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./App.css";
+import { getSumArray } from "./logic/twoSum";
 
 function App() {
   const [formData, setFormData] = useState({
@@ -7,14 +8,43 @@ function App() {
     target: "",
   });
 
+  const [didEdit, setDidEdit] = useState({
+    input: false,
+    target: false,
+  });
+
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [result, setResult] = useState([]);
+  const [displayInput, setDisplayInput] = useState({
+    input: null,
+    target: null,
+  });
+
   function handleSubmit(e) {
     e.preventDefault();
-    console.log(formData.input, formData.target);
+    const inputToArray = formData.input.split(" ").map((item) => Number(item));
+    const inputResult = getSumArray(inputToArray, Number(formData.target));
+
+    setDisplayInput({ input: inputToArray, target: formData.target });
+    setResult(inputResult);
+    setIsSubmitted(true);
   }
 
   function handleChangeInput(identifier, value) {
     setFormData((prev) => ({ ...prev, [identifier]: value }));
+    setDidEdit((prev) => ({ ...prev, [identifier]: false }));
   }
+
+  function handleBlur(identifier) {
+    setDidEdit((prev) => ({ ...prev, [identifier]: true }));
+  }
+
+  const validInputRegex = /^[0-9 ]+$/;
+  const isInputNotValid =
+    didEdit.input && !validInputRegex.test(formData.input);
+
+  const isTargetNotValid =
+    didEdit.target && formData.target.trim().length === 0;
 
   return (
     <div className="container">
@@ -22,6 +52,7 @@ function App() {
         <div className="card-header">
           <h2>Two Sum</h2>
           <h3 className="subtitle">Masukkan input angka dan target angka</h3>
+          <h3 className="subtitle">Dipisahkan menggunakan spasi</h3>
         </div>
         <form className="card-body" onSubmit={handleSubmit}>
           <div className="mb-3">
@@ -33,10 +64,12 @@ function App() {
               className="form-control"
               value={formData.input}
               onChange={(e) => handleChangeInput("input", e.target.value)}
+              onBlur={() => handleBlur("input")}
               id="inputNumber"
               placeholder="Masukkan angka"
             />
           </div>
+          {isInputNotValid && <p className="error-text">Input is not valid</p>}
           <div className="mb-3">
             <label htmlFor="targetNumber" className="form-label">
               Target
@@ -45,14 +78,45 @@ function App() {
               type="number"
               value={formData.target}
               onChange={(e) => handleChangeInput("target", e.target.value)}
+              onBlur={() => handleBlur("target")}
               className="form-control"
               id="targetNumber"
               placeholder="Masukkan target"
             />
+            {isTargetNotValid && (
+              <p className="error-text">Target is required</p>
+            )}
           </div>
           <button className="btn btn-primary btn-submit">Submit</button>
         </form>
       </div>
+      {isSubmitted && (
+        <div className="card">
+          <div className="card-header">
+            <h2>Hasil</h2>
+            <h3 className="subtitle"></h3>
+          </div>
+          <div className="card-body">
+            {result.length > 0 ? (
+              <>
+                <div>Input: {JSON.stringify(displayInput.input)}</div>
+                <div className="mb-3">Target: {displayInput.target}</div>
+                <div className="mb-3">
+                  Pasangan indeks yang menghasilkan jumlah sesuai target yaitu{" "}
+                  <strong>{JSON.stringify(result)}</strong>
+                </div>
+              </>
+            ) : (
+              <>
+                <p className="text-center error-text">
+                  Tidak ditemukan pasangan indeks dari array bilangan bulat
+                </p>
+                <h1 className="text-center">{JSON.stringify(result)}</h1>
+              </>
+            )}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/logic/twoSum.js
@@ -0,0 +1,18 @@
+export function getSumArray(input, target) {
+    let result = [];
+
+    for (let i = 0; i < input.length; i++) {
+        for (let j = i + 1; j < input.length; j++) {
+            if (input[i] + input[j] === target) {
+                result.push([i, j]);
+            }
+        }
+    }
+
+    result.sort((a, b) => {
+        if (a[0] === b[0]) return a[1] - b[1];
+        return a[0] - b[0];
+    });
+
+    return result;
+}
