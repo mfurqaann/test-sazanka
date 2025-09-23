d629d48cc83a91eeeae8f63e6612c94f570635f5
mfurqaann <furqan1061@gmail.com>
2025-09-23 11:24:32 +0700

    feat(twosum): init project

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [input, setInput] = useState([]);
+
+  return <div className="container"></div>;
+}
+
+export default App;
